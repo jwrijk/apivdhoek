605fc4fbe1227142b27c3e68783da071e8b4f373
Simon Rijk <s.p.rijk@gmail.com>
2018-03-25 14:09:44 +0200

    Added led on pin 22

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
+var ppins = require('pi-pins');
+
+var led = ppins.connect(22);
+led.mode('out');
+led.value(true);
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.send('Hello World!! This is going to be awesome.');
+  res.send('Hello World!');
 });
 
 //start a server on port 80 and log its start to our console
-var server = app.listen(process.env.NODE_PORT || 80, function () {
-
+var server = app.listen(80, function () {
   var port = server.address().port;
   console.log('Example app listening on port ', port);
 
 });
+
+// Set On / Off interval
+var ledToggle = false;
+setInterval(function() {
+	ledToggle = !ledToggle;
+	led.value(ledToggle);
+}, 1000)
